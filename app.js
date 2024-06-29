384d820e0a2d0e595843db4ab403b6cbdfa4f981
KimInteger <kjs20151240@gmail.com>
2024-06-29 14:34:55 +0900

    feat_김정수 : express로 server연결

--- app.js
+++ app.js
@@ -7,3 +7,24 @@ const http = require('node:http');
 const fs = require('node:fs');
 
 const path = require('node:path');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use((req,res)=>{
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 Not Found');
+});
+
+app.use((err,req,res,next)=>{
+  console.error(err.message);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 Server error');
+});
+
+app.listen(PORT,()=>{
+  console.log(`http://localhost:${PORT}`);
+});
